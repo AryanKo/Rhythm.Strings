178f94ac4dce303c211c9abca87210166ea3fa50
AryanKo <arkot376@gmail.com>
2026-03-19 03:37:11 +0530

    Update audio tracks

--- data.js
+++ data.js
@@ -4,11 +4,11 @@ export const tracks = [
     { id: 1, name: "Jis Raah Pe", src: "./assets/audio/AUD-20251110-WA0001.m4a", duration: "0:00" },
     { id: 2, name: "Tera Hone Laga Hoon", src: "./assets/audio/AUD-20251120-WA0008.m4a", duration: "0:00" },
     { id: 3, name: "Pasoori", src: "./assets/audio/AUD-20251122-WA0018.m4a", duration: "0:00" },
-    { id: 4, name: "Maand x Jhol x Zara Zara", src: "./assets/audio/AUD-20251129-WA0011.m4a", duration: "0:00" },
+    { id: 4, name: "Maand x Jhol x Zara Zara v1", src: "./assets/audio/AUD-20251129-WA0011.m4a", duration: "0:00" },
     { id: 5, name: "Little Do You Know x Zara Zara", src: "./assets/audio/AUD-20251210-WA0000.m4a", duration: "0:00" },
     { id: 6, name: "Finding Her", src: "./assets/audio/AUD-20251219-WA0006.m4a", duration: "0:00" },
     { id: 7, name: "Hua Hain Aaj Pehli Baar", src: "./assets/audio/AUD-20260106-WA0010.m4a", duration: "0:00" },
-    { id: 8, name: "Zindagi Kuch Toh Bata", src: "./assets/audio/AUD-20260131-WA0013.m4a", duration: "0:00" },
+    { id: 8, name: "Zindagi Kuch Toh Bat v1", src: "./assets/audio/AUD-20260131-WA0013.m4a", duration: "0:00" },
     { id: 9, name: "Subhanallah", src: "./assets/audio/AUD-20260214-WA0007.m4a", duration: "0:00" },
     { id: 10, name: "Zindagi Kuch Toh Bata v2", src: "./assets/audio/AUD-20260214-WA0008.m4a", duration: "0:00" },
     { id: 11, name: "Gehra Hua", src: "./assets/audio/AUD-20260215-WA0007.m4a", duration: "0:00" },
@@ -16,5 +16,9 @@ export const tracks = [
     { id: 13, name: "Yeh Fitoor", src: "./assets/audio/AUD-20260226-WA0002.mp3", duration: "0:00" },
     { id: 14, name: "Unke Andaaz-e-Karam", src: "./assets/audio/AUD-20260301-WA0001.m4a", duration: "0:00" },
     { id: 15, name: "Teri Deewani", src: "./assets/audio/AUD-20260307-WA0013.m4a", duration: "0:00" },
-    { id: 16, name: "Bang Bang", src: "./assets/audio/AUD-20260307-WA0014.m4a", duration: "0:00" }
+    { id: 16, name: "Bang Bang", src: "./assets/audio/AUD-20260307-WA0014.m4a", duration: "0:00" },
+    { id: 17, name: "Sanam Re v1", src: "./assets/audio/AUD-20260318-WA0004.m4a", duration: "0:00" },
+    { id: 18, name: "Zindagi Ke Safar Me", src: "./assets/audio/AUD-20260318-WA0005.m4a", duration: "0:00" },
+    { id: 19, name: "O Jaane Jaa", src: "./assets/audio/AUD-20260318-WA0006.m4a", duration: "0:00" },
+    { id: 20, name: "Sanam Re", src: "./assets/audio/AUD-20260318-WA0007.m4a", duration: "0:00" }
 ];
